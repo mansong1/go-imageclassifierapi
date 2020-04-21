31f92558b069f99ef628c6ea625cf9709e8374bf
mansong1 <martin.ansong@gmail.com>
2020-04-21 08:58:07 +0100

    Add loading func

--- frontend/src/components/PostPrediction.js
+++ frontend/src/components/PostPrediction.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, StrictMode } from 'react';
 import axios from 'axios';
 
-import {
-  Container, Col, Form,
-  FormGroup, Label, Input,
-  Button, FormText, FormFeedback,
-} from 'reactstrap';
 
+import {
+  Container, Col, Form, Row, Label,
+  FormGroup, Button, FormText, FormFeedback, Spinner, Input
+} from 'reactstrap';
 
 const api = axios.create({
   baseURL: `http://localhost:8080`,
@@ -15,13 +14,14 @@ const api = axios.create({
   }
 })
 
-
 class PostPrediction extends Component {
 
   constructor(props) {
     super(props);
 
     this.state = {
+      isLoading: false,
+      prediction: '',
       url: '',
       validate :{
         urlState: '',
@@ -43,50 +43,100 @@ class PostPrediction extends Component {
     this.setState({ [event.target.name]: event.target.value });
   }
 
-  handleSubmit = event => {
+  handleClick= event => {
     event.preventDefault(); //stop browser from reloading page
 
     const URL = {
       url: this.state.url
     }
 
+    this.setState({isLoading: true});
     api
     .post('/classify', URL)
     .then(res => {
-        console.log(res.data)
+      this.setState({
+        prediction: res.data.label,
+        isLoading: false,
+      })
       }
     )
   }
 
+  handleCancelClick = event => {
+    this.setState({ 
+      url: '',
+      prediction: '',
+  });
+  }
+
+  capitaliseString(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
   render() {
-    const { url } = this.state;
+    const url = this.state.url;
+    const isLoading = this.state.isLoading;
+    const prediction = this.state.prediction;
+
     return (
-      <Container fluid >
-        <h2>Image Classifier</h2>
-      <Form className="form" onSubmit={this.handleSubmit}>
-        <Col>
-        <FormGroup>
-          <Label>ImageUrl</Label>
-            <Input
-              name="url"
-              type="url"
-              id="exampleUrl"
-              placeholder="https://image.jpg"
-              value={url}
-              valid={ this.state.validate.urlState === 'has-success' }
-              invalid={ this.state.validate.urlState === 'has-danger' }
-              onChange={this.handleChange}/>
-            <FormText className="text-muted">Image URL should of type jpeg.</FormText>
-            <FormFeedback valid>
-              This looks like a valid URL
-            </FormFeedback>
-            <FormFeedback invalid>
-              This is an invalid URL
-            </FormFeedback>
-        </FormGroup>
-        </Col>
-        <Button color="success" type="submit">Submit</Button>
-      </Form>
+      <Container>
+        <div>
+        <h2 className="title">Image Classifier</h2>
+        </div>
+        <div className="content">
+          <Form>
+            <Row form>
+              <FormGroup>
+                <Label>Provide ImageUrl</Label>
+                <Input
+                  name="url"
+                  type="url"
+                  id="exampleUrl"
+                  placeholder="https://image.jpg"
+                  value={url}
+                  valid={ this.state.validate.urlState === 'has-success' }
+                  invalid={ this.state.validate.urlState === 'has-danger' }
+                  onChange={this.handleChange} />
+                  <FormText className="text-muted">Image URL should be of type jpeg.</FormText>
+                  <FormFeedback valid>
+                    This looks like a valid URL
+                  </FormFeedback>
+                  <FormFeedback invalid>
+                    This is an invalid URL
+                  </FormFeedback>
+              </FormGroup>
+            </Row>
+
+            <Row>
+              <Col>
+                <Button
+                  block
+                  color="success"
+                  type="submit"
+                  disabled={url === "" || this.state.validate.urlState === 'has-danger' }
+                  onClick={!isLoading ? this.handleClick : null}>
+                  { isLoading ? 'Making Prediction' : 'Predict' }
+                </Button>
+              </Col>
+              <Col>
+                <Button
+                  block
+                  color="danger"
+                  disabled={isLoading}
+                  onClick={this.handleCancelClick}>
+                  Reset prediction
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+          {prediction === "" ? null :
+            (<Row>
+              <Col className="result-container">
+                <h5 id="result">This looks like: {this.capitaliseString(prediction)}</h5>
+              </Col>
+            </Row>)
+          }
+        </div>
       </Container>
   );
   }
