ab47e7c64e6e2f79c6a0bde0613625c733275c09
mansong1 <martin.ansong@gmail.com>
2020-05-02 10:50:26 +0100

    add config.js

---
+++ frontend/public/config.js
@@ -0,0 +1,4 @@
+window.ENV = {
+  "REACT_APP_API_URL" : "api-service",
+  "REACT_APP_API_PORT" : "3000"
+}
--- frontend/src/config.js
+++
@@ -1,4 +0,0 @@
-export default {
-    apiServerHost: process.env.REACT_APP_API_URL,
-    apiServerPort: process.env.REACT_APP_API_PORT
-  };
